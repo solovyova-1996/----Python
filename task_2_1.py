d233cae1ba630c41254ba3be2b71b7df05ca737c
solovyova <shatdar@yandex.ru>
2021-02-15 14:10:47 +0600

    create file task_2_1.py

---
+++ task_2_1.py
@@ -0,0 +1,15 @@
+# __author__* Соловьева Дарья Викторовна
+# 1. Выяснить тип результата выражений:
+# 15 * 3
+# 15 / 3
+# 15 // 2
+# 15 ** 2
+
+arithmetic_expression_1 = 15 * 3
+arithmetic_expression_2 = 15 / 3
+arithmetic_expression_3 = 15 // 3
+arithmetic_expression_4 = 15 ** 3
+print("15 * 3", "результат: ", arithmetic_expression_1, type(arithmetic_expression_1))
+print("15 / 3", "результат: ", arithmetic_expression_2, type(arithmetic_expression_2))
+print("15 // 3", "результат: ", arithmetic_expression_3, type(arithmetic_expression_3))
+print("15 ** 3", "результат: ", arithmetic_expression_4, type(arithmetic_expression_4))
